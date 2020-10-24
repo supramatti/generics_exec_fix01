60fe46e90490bbaf3b39c3b8c03263b5c265f26e
Mario <nogueira.mario@gmail.com>
2020-10-24 12:23:53 -0300

    Projeto criado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace generics_exec_fix01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+            s.AddStudents(n);
+            Console.Write("How many students for course B? ");
+            n = int.Parse(Console.ReadLine());
+            s.AddStudents(n);
+            Console.Write("How many students for course c? ");
+            n = int.Parse(Console.ReadLine());
+            s.AddStudents(n);
+            s.Print();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace generics_exec_fix01
+{
+    class Student
+    {
+        public int Id { get; set; }
+        HashSet<Student> set = new HashSet<Student>();
+
+        public Student(int id)
+        {
+            Id = id;
+        }
+
+        public Student()
+        {
+        }
+
+        public void AddStudents(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int id = int.Parse(Console.ReadLine());
+                set.Add(new Student(id));
+            }
+        }
+
+        public void Print()
+        {
+            Console.Write("Total students: " + set.Count);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Student other = obj as Student;
+            return Id.Equals(other.Id);
+        }
+    }
+}
